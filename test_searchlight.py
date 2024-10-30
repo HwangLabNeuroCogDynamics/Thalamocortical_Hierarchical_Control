0b90b288a1e4e2e39460aa8ed1a52d78865206ca
Kai Hwang <kai.hwang@gmail.com>
2024-10-30 12:01:56 -0500

    commit.

--- test_searchlight.py
+++ test_searchlight.py
@@ -17,10 +17,11 @@ from sklearn.metrics import make_scorer
 from sklearn.linear_model import LogisticRegression
 import nilearn
 import datetime
+from nilearn.maskers import NiftiMasker
 
 #import matplotlib.pyplot as plt
 #plt.ion()
-
+permutation = True
 subject = input()
 #subject='10176'
 func_path = "/Shared/lss_kahwang_hpc/data/ThalHi/3dDeconvolve_fdpt4/sub-%s/" %subject
@@ -73,8 +74,13 @@ trial_records = []
 for i, cue in enumerate(cues):
     trial_records = trial_records + [cue] * int(cue_objects[i].shape[3]/2)
 
-mask = index_img(all_cues_img,1)
-mask = new_img_like(mask, (mask.get_fdata()!=0))
+# old whole brain mask
+#mask = index_img(all_cues_img,1)
+#mask = new_img_like(mask, (mask.get_fdata()!=0))
+
+# now thalamus mask, only do decoding on thalamus for now
+mask = nib.load("/Shared/lss_kahwang_hpc/ROIs/Morel_2.5_mask.nii.gz")
+
 
 ### now make the Y 
 context_Y = []
@@ -119,54 +125,89 @@ if version == 'DSFC':
 now = datetime.datetime.now()
 print(now)
 
-mean_fmri = nilearn.image.mean_img(all_cues_img)
-pipeline = make_pipeline(StandardScaler(), LogisticRegression('l1', solver='liblinear')) 
-
-try:
-    searchlight_context = nilearn.decoding.SearchLight(mask, process_mask_img=mask, estimator = pipeline, radius=6, n_jobs=24, verbose=0, cv=LeaveOneGroupOut())
-    searchlight_context.fit(all_cues_img, context_Y, runs)
-    context_score_img = new_img_like(mean_fmri, searchlight_context.scores_)
-    context_score_img.to_filename(func_path + "%s_context_svc_score.nii.gz" %subject)
-    now = datetime.datetime.now()
-    print(now)
-except:
-    print("context broke")
-    pass
-
-try:
+if not permutation:
+    ### below is the real deal
+    mean_fmri = nilearn.image.mean_img(all_cues_img)
     pipeline = make_pipeline(StandardScaler(), LogisticRegression('l1', solver='liblinear')) 
-    searchlight_color = nilearn.decoding.SearchLight(mask, process_mask_img=mask, estimator = pipeline, radius=6, n_jobs=24, verbose=0, cv=LeaveOneGroupOut())
-    searchlight_color.fit(all_cues_img, color_Y, runs)
-    color_score_img = new_img_like(mean_fmri, searchlight_color.scores_)
-    color_score_img.to_filename(func_path + "%s_color_svc_score.nii.gz" %subject)
-    now = datetime.datetime.now()
-    print(now)
-except:
-    print("color broke")
-    pass
 
-try:
-    pipeline = make_pipeline(StandardScaler(), LogisticRegression('l1', solver='liblinear')) 
-    searchlight_shape = nilearn.decoding.SearchLight(mask, process_mask_img=mask, estimator = pipeline, radius=6, n_jobs=24, verbose=0, cv=LeaveOneGroupOut())
-    searchlight_shape.fit(all_cues_img, shape_Y, runs)
-    shape_score_img = new_img_like(mean_fmri, searchlight_shape.scores_)
-    shape_score_img.to_filename(func_path + "%s_shape_svc_score.nii.gz" %subject)
-    now = datetime.datetime.now()
-    print(now)
-except:
-    print("shape broke")
-    pass
+    try:
+        searchlight_context = nilearn.decoding.SearchLight(mask, process_mask_img=mask, estimator = pipeline, radius=6, n_jobs=24, verbose=0, cv=LeaveOneGroupOut())
+        searchlight_context.fit(all_cues_img, context_Y, runs)
+        context_score_img = new_img_like(mean_fmri, searchlight_context.scores_)
+        context_score_img.to_filename(func_path + "%s_context_svc_score.nii.gz" %subject)
+        now = datetime.datetime.now()
+        print(now)
+    except:
+        print("context broke")
+        pass
 
-try:
+    try:
+        pipeline = make_pipeline(StandardScaler(), LogisticRegression('l1', solver='liblinear')) 
+        searchlight_color = nilearn.decoding.SearchLight(mask, process_mask_img=mask, estimator = pipeline, radius=6, n_jobs=24, verbose=0, cv=LeaveOneGroupOut())
+        searchlight_color.fit(all_cues_img, color_Y, runs)
+        color_score_img = new_img_like(mean_fmri, searchlight_color.scores_)
+        color_score_img.to_filename(func_path + "%s_color_svc_score.nii.gz" %subject)
+        now = datetime.datetime.now()
+        print(now)
+    except:
+        print("color broke")
+        pass
+
+    try:
+        pipeline = make_pipeline(StandardScaler(), LogisticRegression('l1', solver='liblinear')) 
+        searchlight_shape = nilearn.decoding.SearchLight(mask, process_mask_img=mask, estimator = pipeline, radius=6, n_jobs=24, verbose=0, cv=LeaveOneGroupOut())
+        searchlight_shape.fit(all_cues_img, shape_Y, runs)
+        shape_score_img = new_img_like(mean_fmri, searchlight_shape.scores_)
+        shape_score_img.to_filename(func_path + "%s_shape_svc_score.nii.gz" %subject)
+        now = datetime.datetime.now()
+        print(now)
+    except:
+        print("shape broke")
+        pass
+
+    try:
+        pipeline = make_pipeline(StandardScaler(), LogisticRegression('l1', solver='liblinear')) 
+        searchlight_task = nilearn.decoding.SearchLight(mask, process_mask_img=mask, estimator = pipeline, radius=6, n_jobs=24, verbose=0, cv=LeaveOneGroupOut())
+        searchlight_task.fit(all_cues_img, task_Y, runs)
+        task_score_img = new_img_like(mean_fmri, searchlight_task.scores_)
+        task_score_img.to_filename(func_path + "%s_task_svc_score.nii.gz" %subject)
+        now = datetime.datetime.now()
+        print(now)
+    except:
+        print("task broke")
+        pass
+
+
+if permutation:
+    ### below is for permutation
+    mean_fmri = nilearn.image.mean_img(all_cues_img)
     pipeline = make_pipeline(StandardScaler(), LogisticRegression('l1', solver='liblinear')) 
-    searchlight_task = nilearn.decoding.SearchLight(mask, process_mask_img=mask, estimator = pipeline, radius=6, n_jobs=24, verbose=0, cv=LeaveOneGroupOut())
-    searchlight_task.fit(all_cues_img, task_Y, runs)
-    task_score_img = new_img_like(mean_fmri, searchlight_task.scores_)
-    task_score_img.to_filename(func_path + "%s_task_svc_score.nii.gz" %subject)
-    now = datetime.datetime.now()
-    print(now)
-except:
-    print("task broke")
-    pass
+
+    try:
+        searchlight_context = nilearn.decoding.SearchLight(mask, process_mask_img=mask, estimator = pipeline, radius=6, n_jobs=24, verbose=0, cv=LeaveOneGroupOut())
+        searchlight_context.fit(all_cues_img, context_Y, runs)
+        context_score_img = new_img_like(mean_fmri, searchlight_context.scores_)
+        #context_score_img.to_filename(func_path + "%s_context_svc_score.nii.gz" %subject)
+        thalamus_context_decoding = NiftiMasker(mask).fit_transform(context_score_img)
+        np.save("/Shared/lss_kahwang_hpc/data/ThalHi/decoding/permutation/%s_context_decoding.npy" %subject, thalamus_context_decoding)
+
+
+        #now the permutation
+        perumted_thalamus_context_decoding = np.zeros((500, 2473))
+        for i in range(500):
+            np.random.shuffle(context_Y)
+            searchlight_context = nilearn.decoding.SearchLight(mask, process_mask_img=mask, estimator = pipeline, radius=6, n_jobs=24, verbose=0, cv=LeaveOneGroupOut())
+            searchlight_context.fit(all_cues_img, context_Y, runs)
+            context_score_img = new_img_like(mean_fmri, searchlight_context.scores_)
+            #context_score_img.to_filename(func_path + "%s_context_svc_score.nii.gz" %subject)
+            perumted_thalamus_context_decoding[i,:] = NiftiMasker(mask).fit_transform(context_score_img)[0]
+        
+        np.save("/Shared/lss_kahwang_hpc/data/ThalHi/decoding/permutation/%s_context_decoding_permutation.npy" %subject, perumted_thalamus_context_decoding)        
+
+        now = datetime.datetime.now()
+        print(now)
+    except:
+        print("context broke")
+        pass
 
 ### end
